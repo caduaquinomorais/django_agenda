779f060696e2692060d9d9e204863be5fde3e11e
caduaquinomorais <caduaquino@yahoo.com>
2025-02-13 18:54:14 -0300

    467 - Usando html pra organizar a lista

--- contact/admin.py
+++ contact/admin.py
@@ -5,12 +5,12 @@ from contact.models import Contact
 #configuração do model na admin do django
 @admin.register(models.Contact)
 class ContactAdmin (admin.ModelAdmin):
-    list_display = ('id','first_name','last_name','phone',)
+    list_display = ('id','first_name','last_name','phone','show')
     ordering= ('-id',)#ordenando por id contrário para os mais novos ficarem no começo
     search_fields = ('id','first_name',)
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name','show'
     list_display_links = 'phone',
     
     
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -5,8 +5,8 @@ from contact.models import Contact
 
 
 def index(request):
-    contacts = Contact.objects.all()
-    
+    contacts = Contact.objects.all().order_by('-id')#Visualizar o id pelo ultimo
+    #no lugar do all, posso usar o filter(show=true) onde o show true é uma escolha na pagina do admin pra mostrar ou não o contato. Além disso, posso colocar outros filtros
     context = {
         'contacts': contacts
     }
